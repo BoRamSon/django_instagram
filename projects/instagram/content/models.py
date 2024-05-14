80fc6d8f2bc690dcb50fcb3dd0d412e0016c485a
BoRamSon <onbrson1@gmail.com>
2024-05-14 12:07:45 +0900

    create : content folder and user folder

---
+++ projects/instagram/content/models.py
@@ -0,0 +1,12 @@
+# content/models.py
+
+from django.db import models
+
+
+class Feed(models.Model):
+    content = models.TextField()  # 글 내용
+    image = models.TextField()  # 피드 이미지
+    profile_image = models.TextField()  # 프로필 이미지
+    user_id = models.TextField()  # 글쓴이
+    like_count = models.PositiveIntegerField()  # 좋아요 수
+
