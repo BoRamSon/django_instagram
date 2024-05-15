42c3a1f52293c8f31f939c03cb3026a5eacfa1a7
BoRamSon <onbrson1@gmail.com>
2024-05-16 00:46:26 +0900

    feat : 서버로 파일 업로드 하는 API 만들기 (Back-end 작업완료) ㄴ 2단계 : 피드 업로드 기능 구현 - 백엔드 (Django) ㄴ front에서 event 작업을 해주었으니 이제는 백엔드에서 event에 따라서 데이터를 받게 하기 위한 작업을 해주자. ㄴ content/views.py 작업  	ㄴ 가져온 이미지나 내용을 처리할 수 있도록 ㄴ config/urls.py 작업 ㄴ 403 문제해결을 위해 index.html에 csrf_token 추가 ㄴ 원활한 통신을 위해서 file와 ajax 코드수정 ㄴ Front 에서 보낸 csrftoken 을 처리해주기 위해서 content/views.py 코드를 아래와 같이 수정해주세요.

--- projects/instagram/config/urls.py
+++ projects/instagram/config/urls.py
@@ -1,5 +1,3 @@
-# config/urls.py
-
 """
 URL configuration for config project.
 
@@ -19,10 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from content.views import Index, UploadFeed  # 추가
+from content.views import Index, UploadFeed
+from django.conf import settings  # 추가
+from django.conf.urls.static import static  # 추가
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("index/", Index.as_view()),
-    path("content/upload", UploadFeed.as_view()),  # 추가
+    path("content/upload", UploadFeed.as_view()),
 ]
+
+# 추가
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/instagram/content/views.py
+++ projects/instagram/content/views.py
@@ -1,25 +1,40 @@
-# content/views.py
-
+from uuid import uuid4
 from django.shortcuts import render
-from rest_framework.response import Response  # 추가
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Feed
+import os
+from config.settings import MEDIA_ROOT
+from django.views.decorators.csrf import csrf_exempt  # 403 Forbidden Error Sol
+from django.utils.decorators import method_decorator  # 403 Forbidden Error Sol
 
 
 class Index(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all()  # select * from content_feed;
+        feed_list = Feed.objects.all().order_by("-id")  # select * from content_feed;
         return render(request, "instagram/index.html", context=dict(feeds=feed_list))
 
 
-# 추가
+@method_decorator(csrf_exempt, name="dispatch")  # 403 Forbidden Error Sol
 class UploadFeed(APIView):
     def post(self, request):
-
-        file = request.data.get("file")
-        image = request.data.get("image")
+        file = request.FILES["file"]
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+        with open(save_path, "wb+") as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
         content = request.data.get("content")
-        user_id = request.data.get("user_id")
+        image = uuid_name
         profile_image = request.data.get("profile_image")
+        user_id = request.data.get("user_id")
+
+        Feed.objects.create(
+            content=content,
+            image=image,
+            profile_image=profile_image,
+            user_id=user_id,
+            like_count=0,
+        )
 
         return Response(status=200)
